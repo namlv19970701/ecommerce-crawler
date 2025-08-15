26ae2459a31d981b3dc2e17661803ab40c769d2c
namlv19970701 <namlv.19970701@gmail.com>
2025-08-15 11:55:18 +0700

    Update crawl_product_by_category.py

--- dags/tiki/crawl_product_by_category.py
+++ dags/tiki/crawl_product_by_category.py
@@ -5,6 +5,8 @@ from utils import request_proxy
 import pendulum
 from airflow.decorators import task
 import json
+from airflow.providers.apache.kafka.hooks.produce import KafkaProducerHook
+from datetime import datetime 
 
 tiki_category_curl = Variable.get("tiki_category_curl")
 tiki_product_by_category_curl = Variable.get("tiki_product_by_category_curl")
@@ -40,7 +42,12 @@ with DAG(
         url_key=link.split("/")[-2]
         category_id=link.split("/")[-1][1:]
         print(url_key,category_id)
-        
+
+        producer = KafkaProducerHook(
+            kafka_config_id=kafka_connection,  # Make sure you configure the Kafka connection in Airflow
+        ).get_producer()
+
+        total_items=0
         product_curl=tiki_product_by_category_curl
         for page in range(1,2):
             values={
@@ -56,10 +63,29 @@ with DAG(
             result=subprocess.run(curl_command,shell=True,text=True,capture_output=True)
             response=json.loads(result.stdout)
 
-            return response.get('data',[])
+            data=response.get('data',[])
 
-    
+            for item in data:
+                item.update({
+                    "crawled_at":int(datetime.now().timestamp()*1000),
+                    "category_id":category_id,
+                    "category_name":url_key
+                })
+                producer.produce(
+                    topic="crawl-tiki-product",
+                    value=json.dumps(item).encode("utf8"),
+                )
+                total_items+=1
+            producer.flush()
+            break
+
+        return total_items
+
+    @task
+    def sum_total_crawled_products(total_items):
+        return sum(total_items)
         
-
+        
     categories=get_category()
     products=get_products.expand(category=categories)
+    sum_total_crawled_products(products)
