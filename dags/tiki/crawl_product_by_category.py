61597c88103807a6e79fab12164d0f14171d68d6
namlv19970701 <namlv.19970701@gmail.com>
2025-08-22 11:51:10 +0700

    Update crawl_product_by_category.py

--- dags/tiki/crawl_product_by_category.py
+++ dags/tiki/crawl_product_by_category.py
@@ -7,6 +7,7 @@ from airflow.decorators import task
 import json
 from airflow.providers.apache.kafka.hooks.produce import KafkaProducerHook
 from datetime import datetime 
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 tiki_category_curl = Variable.get("tiki_category_curl")
 tiki_product_by_category_curl = Variable.get("tiki_product_by_category_curl")
@@ -43,10 +44,10 @@ with DAG(
         category_id=link.split("/")[-1][1:]
         print(url_key,category_id)
 
-        producer = KafkaProducerHook(
-            kafka_config_id="kafka",  # Make sure you configure the Kafka connection in Airflow
-        ).get_producer()
-
+        # producer = KafkaProducerHook(
+        #     kafka_config_id="kafka",  # Make sure you configure the Kafka connection in Airflow
+        # ).get_producer()
+        client = S3Hook(aws_conn_id=s3_connection)
         total_items=0
         product_curl=tiki_product_by_category_curl
         for page in range(1,2):
@@ -62,21 +63,36 @@ with DAG(
             curl_command=curl_command.split("\\")
             result=subprocess.run(curl_command,shell=True,text=True,capture_output=True)
             response=json.loads(result.stdout)
+            crawled_at=int(datetime.now().timestamp()*1000)
+            
+            data={'data':response.get('data',[])}
+            
+            data.update({
+                "crawled_at":int(datetime.now().timestamp()*1000),
+                "category_id":category_id,
+                "category_name":url_key
+            })
+                
+            client.load_string(
+                string_data=json.dumps(data),
+                key="raw/tiki/",  # đường dẫn trong bucket
+                bucket_name="test",    # tên bucket
+                replace=True                # ghi đè nếu tồn tại
+            )
 
-            data=response.get('data',[])
-
-            for item in data:
-                item.update({
-                    "crawled_at":int(datetime.now().timestamp()*1000),
-                    "category_id":category_id,
-                    "category_name":url_key
-                })
-                producer.produce(
-                    topic="crawl-tiki-product",
-                    value=json.dumps(item).encode("utf8"),
-                )
-                total_items+=1
-            producer.flush()
+            total_items+=len(data['data'])
+            # for item in data:
+            #     item.update({
+            #         "crawled_at":int(datetime.now().timestamp()*1000),
+            #         "category_id":category_id,
+            #         "category_name":url_key
+            #     })
+            #     producer.produce(
+            #         topic="crawl-tiki-product",
+            #         value=json.dumps(item).encode("utf8"),
+            #     )
+            #     total_items+=1
+            # producer.flush()
 
         return total_items
 
